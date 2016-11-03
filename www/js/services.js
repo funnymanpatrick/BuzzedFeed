03b9a132c93ee33ecf5136545281d33b4a164148
Patrick Hesselbach <hessep@rpi.edu>
2016-11-02 21:07:37 -0400

    updated stock stuff to be less static

--- www/js/services.js
+++ www/js/services.js
@@ -1,3 +1,32 @@
+var d = [
+			{name: 'Gin and Tonic', alcohol: ['Gin'], mixers: ['Tonic'], rating: 5.0},
+			{name: 'Dark and Stormy', alcohol: ['Rum'], mixers: ['Ginger Beer'], rating: 5.0},
+			{name: 'Rum and Coke', alcohol: ['Rum'], mixers: ['Cola'], rating: 5.0},
+			{name: 'Bloody Mary', alcohol: ['Vodka'], mixers: ['Tomato Juice'], rating: 5.0},
+			{name: 'Moscow Mule', alcohol: ['Vodka'], mixers: ['Ginger Beer', 'Lime Juice'], rating: 5.0},
+			{name: 'Screwdriver', alcohol: ['Vodka'], mixers: ['Orange Juice'], rating: 4.1},
+			{name: 'Long Island Iced Tea', alcohol: ['Gin', 'Tequila', 'Vodka', 'Rum'], mixers: ['Lemon Juice'], rating: 5.0}
+		];
+
+var a = [
+			{name: 'Vodka', ammount: 5},
+			{name: 'Rum', ammount: 2},
+			{name: 'Gin', ammount: 1},
+			{name: 'Tequila', ammount: 7},
+			{name: 'Whiskey', ammount: 0}
+		];
+
+var m = [
+			{name: 'Tonic Water', ammount: 4},
+			{name: 'Ginger Beer', ammount: 5},
+			{name: 'Cola', ammount: 5},
+			{name: 'Tomato Juice', ammount: 5},
+			{name: 'Orange Juice', ammount: 5},
+			{name: 'Red Bull', ammount: 5},
+			{name: 'Lemon Juice', ammount: 5},
+			{name: 'Lime Juice', ammount: 5}
+		];
+
 angular.module('app.services', [])
 
 
@@ -7,41 +36,18 @@ angular.module('app.services', [])
 
 .factory('GetDrinks', [function() {
 	return {
-		drinks: [
-			{name: 'Gin and Tonic', alcohol: ['Gin'], mixers: ['Tonic'], rating: 5.0},
-			{name: 'Dark and Stormy', alcohol: ['Rum'], mixers: ['Ginger Beer'], rating: 5.0},
-			{name: 'Rum and Coke', alcohol: ['Rum'], mixers: ['Cola'], rating: 5.0},
-			{name: 'Bloody Mary', alcohol: ['Vodka'], mixers: ['Tomato Juice'], rating: 5.0},
-			{name: 'Moscow Mule', alcohol: ['Vodka'], mixers: ['Ginger Beer', 'Lime Juice'], rating: 5.0},
-			{name: 'Screwdriver', alcohol: ['Vodka'], mixers: ['Orange Juice'], rating: 4.1},
-			{name: 'Long Island Iced Tea', alcohol: ['Gin', 'Tequila', 'Vodka', 'Rum'], mixers: ['Lemon Juice'], rating: 5.0}
-		]
+		drinks: d
 	};
 }])
 
 .factory('GetAlcohol', [function(){
 	return {
-		alcohol: [
-			{name: 'Vodka', ammount: 5},
-			{name: 'Rum', ammount: 2},
-			{name: 'Gin', ammount: 1},
-			{name: 'Tequila', ammount: 7},
-			{name: 'Whiskey', ammount: 0}
-		]
-	}
+		alcohol: a
+	};
 }])
 
 .factory('GetMixers', [function(){
 	return {
-		mixers: [
-			{name: 'Tonic Water', ammount: 5},
-			{name: 'Ginger Beer', ammount: 5},
-			{name: 'Cola', ammount: 5},
-			{name: 'Tomato Juice', ammount: 5},
-			{name: 'Orange Juice', ammount: 5},
-			{name: 'Red Bull', ammount: 5},
-			{name: 'Lemon Juice', ammount: 5},
-			{name: 'Lime Juice', ammount: 5}
-		]
-	}
+		mixers: m
+	};
 }]);
